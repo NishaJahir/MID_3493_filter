9aa5d9be0d329072285b34de099642b5082420c1
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-03-24 16:31:19 +0530

    Update NovalnetPaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
@@ -123,7 +123,7 @@ class NovalnetPaymentMethodReinitializePayment
     
       $paymentHelper->logger('tid status', $tid_status);
       $paymentHelper->logger('tid key', $paymentKey);
-       $paymentHelper->logger('isPaymentRequestSendNNNNN', $isPaymentRequestSend);
+       $paymentHelper->logger('isPaymentRequestSendNNNNNSSSSSSSSSS', $isPaymentRequestSend);
     
        // If the Novalnet payments are rejected do the reinitialize payment
        if( strpos($paymentKey, 'NOVALNET') !== false &&  ( (!empty($tid_status) && !in_array($tid_status, [75, 85, 86, 90, 91, 98, 99, 100, 103])) || (empty($tid_status) && $isPaymentRequestSend != true) )) {
@@ -143,7 +143,8 @@ class NovalnetPaymentMethodReinitializePayment
             'nnGuaranteeStatus' => $show_birthday,
             'orderAmount' => $orderAmount,
             'billingAddressId' => $order['billingAddress']['id'],
-            'shippingAddressId' => $order['deliveryAddress']['id']
+            'shippingAddressId' => $order['deliveryAddress']['id'],
+            'isPaymentRequestSend' => $isPaymentRequestSend
           ]);
        } else {
           return '';
